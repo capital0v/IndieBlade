21dd36b7910e6e0021021c366a1417c5dec1eabf
Danil <danyacapital@gmail.com>
2025-03-21 18:40:30 +0300

    Indie Blade 1.1.0
    Updated MemoryRaider library

--- IndieBlade/Main.cs
+++ IndieBlade/Main.cs
@@ -1,4 +1,6 @@
 ﻿using Capitalov;
+using System;
+using System.Collections.Generic;
 
 namespace IndieBlade
 {
@@ -6,8 +8,6 @@ namespace IndieBlade
     {
         private MemoryRaider _memory = new MemoryRaider();
         private string _logo = @"
-
-
                   ██ ███▄    █ ▓█████▄   ██▓ ▓█████      ▄▄▄▄     ██▓    ▄▄▄     ▓█████▄  ▓█████
                 ▒▓██ ██ ▀█   █ ▒██▀ ██▌▒▓██▒ ▓█   ▀     ▓█████▄  ▓██▒   ▒████▄   ▒██▀ ██▌ ▓█   ▀
                 ░▒██▓██  ▀█ ██▒░██   █▌▒▒██▒ ▒███       ▒██▒ ▄██ ▒██░   ▒██  ▀█▄ ░██   █▌ ▒███  
@@ -17,8 +17,6 @@ namespace IndieBlade
                   ▒ ░ ░░   ░ ▒░ ░ ▒  ▒ ░ ▒ ░░ ░ ░      ░▒░▒   ░ ░░ ░ ▒  ░ ░   ▒▒  ░ ▒  ▒ ░ ░ ░  
                   ▒    ░   ░ ░  ░ ░  ░ ░ ▒ ░    ░        ░    ░    ░ ░    ░   ▒   ░ ░  ░     ░  
                   ░          ░    ░      ░  ░   ░      ░ ░      ░    ░        ░     ░    ░   ░  
-
-
 ";
         private string _space = "                                   ";
 
@@ -27,23 +25,47 @@ namespace IndieBlade
             Console.WriteLine(_logo);
             Console.WriteLine($"{_space}Write the name of the game's process: ");
             string process = Console.ReadLine();
-            _memory.Inject(process);
+            _memory.Attach(process);
+
             Console.Clear();
             Console.WriteLine(_logo);
             Console.WriteLine($"{_space}Do you know the meaning of gravity in the game? (y/n)");
             string knows = Console.ReadLine();
+
+            float gravity = knows.ToLower() == "y" ? GetGravityValue() : -9.81f;
+
             Console.Clear();
             Console.WriteLine(_logo);
-            float gravity = knows == "y" ? GetGravityValue() : -9.81f;
             Console.WriteLine($"{_space}Write the new value of gravity: ");
             float newGravity = float.Parse(Console.ReadLine());
-            _memory.ChangeValue(gravity, newGravity);
+
+            ChangeGravity(gravity, newGravity);
         }
 
         private float GetGravityValue()
         {
             Console.WriteLine($"{_space}Write the value of gravity: ");
             return float.Parse(Console.ReadLine());
+        }
+
+        private void ChangeGravity(float gravity, float newGravity)
+        {
+            Dictionary<IntPtr, float> foundValues = _memory.ScanValue(gravity);
+
+            if (foundValues.Count == 0)
+            {
+                Console.WriteLine($"{_space}No values matching gravity = {gravity} were found.");
+                return;
+            }
+
+            Console.WriteLine($"{_space}Found {foundValues.Count} addresses with gravity = {gravity}.");
+
+            foreach (var address in foundValues.Keys)
+            {
+                _memory.Write(address, newGravity);
+            }
+
+            Console.WriteLine($"{_space}Gravity has been changed to {newGravity} at {foundValues.Count} addresses.");
         }
     }
 }
